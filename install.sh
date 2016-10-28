81f9c84b5eb7cd40cf44eef54a789e081fab1df2
Cristian Mello <cristianc.mello@gmail.com>
2016-10-28 19:19:32 -0200

    alter script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# args: $1 = [--gpu | --cpu]
 
 source env.sh
 
@@ -7,19 +8,19 @@ function server-install-msg-error
 	echo $SERVER_MSG_ERROR
 }
 
-function server-install-start-gpu
+function server-install-or-start-gpu
 {
 	eval $NVIDIA_DOCKER_INSTALLER
 	eval $TENSORFLOW_START_SERVER $1 $2
 }
 
-function server-install-start-cpu
+function server-install-or-start-cpu
 {
 	eval $TENSORFLOW_START_SERVER $1 $2
 }
 
 # Função para construir servidor a partir de um Dockerfile
-function install-start-server
+function install-or-start-server
 {
 	# Se o segundo argumento tiver algo escrito, pare o script.
 	if [[ !(-z "${2+set}") ]]; then
@@ -28,21 +29,21 @@ function install-start-server
 	fi
 
 	if [[ "$1" == $TENSORFLOW_SERVER_ARG1_GPU ]]; then
-		server-install-start-gpu $1 $2
+		server-install-or-start-gpu $1 $2
 	elif [[ "$1" == $TENSORFLOW_SERVER_ARG1_CPU ]]; then
-		server-install-start-cpu $1 $2
+		server-install-or-start-cpu $1 $2
 	else
 		if [[ !(-z "${1+set}") ]]; then
 			server-install-msg-error
 			exit
 		fi
 		
-		server-install-start-cpu $1 $2
+		server-install-or-start-cpu $1 $2
 	fi
 }
 
 eval $DOCKER_INSTALLER
-install-start-server $1 $2
+install-or-start-server $1 $2
 
 
 
