10e3818587e260a4b5be6d1d0308d06479bb3787
Cristian Mello <cristianc.mello@gmail.com>
2016-12-06 11:39:00 -0200

    adicionando pasta de tutorial

---
+++ mnist-train-1/tutorial/Tensorflow, MNIST and your own handwritten digits_files/index.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 3:44:37 abr 5, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 15:04:11 nov 2, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+        
+        // Calculates Reading Time
+        $('.post-content').readingTime({
+            readingTimeTarget: '.post-reading-time',
+            wordCountTarget: '.post-word-count',
+        });
+        
+        // Creates Captions from Alt tags
+        $(".post-content img").each(function() {
+            // Let's put a caption if there is one
+            if($(this).attr("alt"))
+              $(this).wrap('<figure class="image"></figure>')
+              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+        });
+        
+    });
+
+}(jQuery));
